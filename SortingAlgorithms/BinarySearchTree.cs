ee8a379543da5ee96f0e75dbcff1a9ad56c24044
NCRadtke00 <raddnc@gmail.com>
2020-09-03 09:57:11 -0500

    Project was very difficult to get started, spent time rereading lecture note and Read Microsoft docs. Was originally trying to use before and aafter to add to the BST but realized it was flawed and with some help adjusted it to current. porject done

---
+++ SortingAlgorithms/BinarySearchTree.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    class BinarySearchTree
+    {
+        public Node Root { get; set; }
+        public void Add(int d)
+        {
+            if (this.Root == null)
+            {
+                this.Root = new Node(d);
+            }
+            else
+            {
+                Node currentNode = this.Root;
+                while (currentNode != null)
+                {
+                    if(d <= currentNode.Data)
+                    {
+                        currentNode = currentNode.left;
+                    }
+                    else if (d > currentNode.Data)
+                    {
+                        currentNode = currentNode.right;
+                    }
+                }
+                currentNode = new Node(d);
+            }
+
+        }
+        public Node Search(int d)
+        {
+            return this.Search(d, this.Root);
+        }
+        private Node Search(int d, Node currentNode)
+        {
+            if (currentNode != null)
+            {
+                if (d == currentNode.Data)
+                {
+                    return currentNode;
+                }
+                else if (d <= currentNode.Data)
+                {
+                    return Search(d, currentNode.left);
+                }
+                else if (d > currentNode.Data)
+                {
+                    return Search(d, currentNode.right);
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ SortingAlgorithms/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    class Node
+    {
+        public int Data;
+        public Node left { get; set; }
+        public Node right { get; set; }
+        public Node(int data)
+        {
+            this.Data = data;
+        }
+    }
+}
